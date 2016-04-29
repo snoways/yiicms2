503ff41b9c1fe834a9f6a089ebdf6f86a92bde61
dmodaii <dzm1846907072@gmail.com>
2016-04-29 17:05:10 +0800

    init

---
+++ config/db.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=yiicms2',
+    'username' => 'root',
+    'password' => 'waxd32123',
+    'charset' => 'utf8',
+    'tablePrefix'=>'yii_'
+];
