81f8f4673b3fb859866e79a38da30e23eb9a24fc
dmodaii <dzm1846907072@gmail.com>
2016-05-06 10:32:22 +0800

    修改数据库配置

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yiicms2',
+    'dsn' => 'mysql:host=localhost;dbname=yii_cms2',
     'username' => 'root',
     'password' => 'waxd32123',
     'charset' => 'utf8',
